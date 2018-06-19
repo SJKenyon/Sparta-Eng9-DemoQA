ed01715717904caf6b02c62f80a018f62be56201
SJKenyon <skenyon@spartaglobal.com>
2018-06-19 18:41:36 +0100

    email variable changed

--- features/lib/pages/demo_qa_account.rb
+++ features/lib/pages/demo_qa_account.rb
@@ -7,7 +7,7 @@ class AccountPage
   HOMEPAGE_URL = "http://store.demoqa.com/"
   LOGIN_URL = "http://store.demoqa.com/wp-login.php"
   SUCCESSFUL_REGISTRATION = "Registration complete. Please check your email."
-  WRONG_PASSWORD = "ERROR: The password you entered for the email address #{EMAIL} is incorrect. Lost your password?"
+  WRONG_PASSWORD = "ERROR: The password you entered for the email address ??? is incorrect. Lost your password?"
   NO_USERNAME = "ERROR: The username field is empty."
   NO_PASSWORD = "ERROR: The password field is empty."
 
