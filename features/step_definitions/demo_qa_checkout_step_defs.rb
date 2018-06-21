966ab7a669a47af9cedcf3650edb4b252b5fd1ac
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-06-21 12:34:09 +0100

    Added step defs for next scenario

--- features/step_definitions/demo_qa_checkout_step_defs.rb
+++ features/step_definitions/demo_qa_checkout_step_defs.rb
@@ -9,3 +9,20 @@ end
 Then("I should see the correct error message") do
   demo_qa_checkout.check_error_message
 end
+
+Given("that I am on an individual product page") do
+  
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click on add to cart") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click on the checkout button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the subtotal of my basket on the checkout page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
