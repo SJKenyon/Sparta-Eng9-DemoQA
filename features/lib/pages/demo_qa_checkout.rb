b0435c5645840ef09dc54dbdb1f272df8142781e
SJKenyon <skenyon@spartaglobal.com>
2018-06-22 13:22:16 +0100

    fill out form correctly

--- features/lib/pages/demo_qa_checkout.rb
+++ features/lib/pages/demo_qa_checkout.rb
@@ -10,6 +10,9 @@ class CheckoutPage
   @@checkout_h1 = 'Checkout'
   @@subtotal_span = 'Sub-Total:'
 
+  def visit_checkout_url
+    visit(CHECKOUT_URL)
+  end
 
   def check_checkout_page
     page.has_content?(@@checkout_h1)
@@ -31,6 +34,24 @@ class CheckoutPage
     page.has_content?(@@subtotal_span)
   end
 
+  def click_continue
+    click_link("Continue")
+  end
 
+  def fill_out_form_correctly
+    find(:id, "wpsc_checkout_form_9").send_keys("example@gmail.com")
+    find(:id, "wpsc_checkout_form_2").send_keys("example")
+    find(:id, "wpsc_checkout_form_3").send_keys("sparta")
+    find(:id, "wpsc_checkout_form_4").send_keys("some random street")
+    find(:id, "wpsc_checkout_form_5").send_keys("city")
+    find(:id, "wpsc_checkout_form_6").send_keys("dont know")
+    # find("#wpsc_checkout_form_7").find(:xpath, 'option[2]').select_option
+    find(:id, "wpsc_checkout_form_18").send_keys("07856434567")
+    find(:id, "shippingSameBilling").click
+  end
+
+  def click_purchase
+    find(".input-button-buy").click
+  end
 
 end #end of class
