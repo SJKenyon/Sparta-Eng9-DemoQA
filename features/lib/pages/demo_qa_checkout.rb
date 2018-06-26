f692a60c603bfc3641df6e1fc3006fded67414e9
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-06-26 11:07:56 +0100

    Changed all constants and class vars to instance vars in account.rb and checkout.rb

--- features/lib/pages/demo_qa_checkout.rb
+++ features/lib/pages/demo_qa_checkout.rb
@@ -5,37 +5,39 @@ class CheckoutPage
 
   include Capybara::DSL
 
-  HOMEPAGE_URL = "http://store.demoqa.com/"
-  CHECKOUT_URL = "http://store.demoqa.com/products-page/checkout/"
-  @@checkout_error_message = 'Oops, there is nothing in your cart.'
-  @@checkout_h1 = 'Checkout'
-  @@subtotal_span = 'Sub-Total:'
-  @@continue_button = 'Continue'
-  #Instantiating the random generator
-  @@random_generator = RandomFormValues.new
-  #Form fields IN CHECKOUT
-  @@purchase_button = 'Purchase'
-  @@calculate_button = 'Calculate'
-  @@email_field = 'wpsc_checkout_form_9'
-  @@first_name_field = 'wpsc_checkout_form_2'
-  @@surname_field = 'wpsc_checkout_form_3'
-  @@address_field = 'wpsc_checkout_form_4'
-  @@city_field = 'wpsc_checkout_form_5'
-  @@region_field = 'wpsc_checkout_form_6'
-  @@postcode_field = 'wpsc_checkout_form_8'
-  @@phone_num_field = 'wpsc_checkout_form_18'
-  @@billing_address_checkbox = 'shippingSameBilling'
+  def initialize
+    @homepage_url = "http://store.demoqa.com/"
+    @checkout_url = "http://store.demoqa.com/products-page/checkout/"
+    @checkout_error_message = 'Oops, there is nothing in your cart.'
+    @checkout_h1 = 'Checkout'
+    @subtotal_span = 'Sub-Total:'
+    @continue_button = 'Continue'
+    #Instantiating the random generator
+    @random_generator = RandomFormValues.new
+    #Form fields IN CHECKOUT
+    @purchase_button = 'Purchase'
+    @calculate_button = 'Calculate'
+    @email_field = 'wpsc_checkout_form_9'
+    @first_name_field = 'wpsc_checkout_form_2'
+    @surname_field = 'wpsc_checkout_form_3'
+    @address_field = 'wpsc_checkout_form_4'
+    @city_field = 'wpsc_checkout_form_5'
+    @region_field = 'wpsc_checkout_form_6'
+    @postcode_field = 'wpsc_checkout_form_8'
+    @phone_num_field = 'wpsc_checkout_form_18'
+    @billing_address_checkbox = 'shippingSameBilling'
+  end
 
   def visit_checkout_url
-    visit(CHECKOUT_URL)
+    visit(@checkout_url)
   end
 
   def check_checkout_page
-    page.has_content?(@@checkout_h1)
+    page.has_content?(@checkout_h1)
   end
 
   def check_error_message
-    page.has_content?(@@checkout_error_message)
+    page.has_content?(@checkout_error_message)
   end
 
   def find_checkout_quantity
@@ -51,19 +53,19 @@ class CheckoutPage
   end
 
   def check_subtotal
-    page.has_content?(@@subtotal_span)
+    page.has_content?(@subtotal_span)
   end
 
   def click_continue_button
-    find_link(@@continue_button).click
+    find_link(@continue_button).click
   end
 
   def fill_shipping_country
-    select(@@random_generator.random_country_from_array, :from => 'country', visible: false)
+    select(@random_generator.random_country_from_array, :from => 'country', visible: false)
   end
 
   def click_calculate
-    click_on(@@calculate_button)
+    click_on(@calculate_button)
   end
 
   def check_shipping_error_message
@@ -72,47 +74,47 @@ class CheckoutPage
 
   #Filling in the email and billing details part of the form
   def fill_email_field
-    fill_in(@@email_field, :with => @@random_generator.generate_email_address)
+    fill_in(@email_field, :with => @random_generator.generate_email_address)
   end
 
   def fill_first_name_field
-    fill_in(@@first_name_field, :with => @@random_generator.generate_first_name)
+    fill_in(@first_name_field, :with => @random_generator.generate_first_name)
   end
 
   def fill_surname_field
-    fill_in(@@surname_field, :with => @@random_generator.generate_last_name)
+    fill_in(@surname_field, :with => @random_generator.generate_last_name)
   end
 
   def fill_address_field
-    fill_in(@@address_field, :with => @@random_generator.generate_address)
+    fill_in(@address_field, :with => @random_generator.generate_address)
   end
 
   def fill_city_field
-    fill_in(@@city_field, :with => @@random_generator.generate_city)
+    fill_in(@city_field, :with => @random_generator.generate_city)
   end
 
   def fill_region_field
-    fill_in(@@region_field, :with => @@random_generator.generate_region)
+    fill_in(@region_field, :with => @random_generator.generate_region)
   end
 
   def fill_postcode_field
-    fill_in(@@postcode_field, :with => @@random_generator.generate_postcode)
+    fill_in(@postcode_field, :with => @random_generator.generate_postcode)
   end
 
   def fill_billing_country
-    select(@@random_generator.random_country_from_array, :from => 'wpsc_checkout_form_7', visible: false)
+    select(@random_generator.random_country_from_array, :from => 'wpsc_checkout_form_7', visible: false)
   end
 
   def fill_phone_num_field
-    fill_in(@@phone_num_field, :with => @@random_generator.generate_phone_number)
+    fill_in(@phone_num_field, :with => @random_generator.generate_phone_number)
   end
 
   def tick_billing_address_box
-    check(@@billing_address_checkbox)
+    check(@billing_address_checkbox)
   end
 
   def click_purchase
-    click_on(@@purchase_button)
+    click_on(@purchase_button)
   end
 
   def check_successful_purchase
@@ -137,27 +139,27 @@ class CheckoutPage
 
 #Checking that error messages are working correctly
   def input_invalid_email
-    fill_in(@@email_field, :with => @@random_generator.generate_invalid_username)
+    fill_in(@email_field, :with => @random_generator.generate_invalid_username)
   end
 
   def input_invalid_first_name
-    fill_in(@@first_name_field, :with => "")
+    fill_in(@first_name_field, :with => "")
   end
 
   def input_invalid_surname
-    fill_in(@@surname_field, :with => "")
+    fill_in(@surname_field, :with => "")
   end
 
   def input_invalid_address
-    fill_in(@@address_field, :with => "")
+    fill_in(@address_field, :with => "")
   end
 
   def input_invalid_city
-    fill_in(@@city_field, :with => "")
+    fill_in(@city_field, :with => "")
   end
 
   def input_invalid_phone_num
-    fill_in(@@phone_num_field, :with => "")
+    fill_in(@phone_num_field, :with => "")
   end
 
   def check_email_error
